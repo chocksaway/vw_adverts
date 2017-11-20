b2408358c05b7d12cdacf506f65c2627dd3b9b88
chocksaway <milesd@chocksaway.com>
2017-11-20 21:57:15 +0000

    Simple format change.

--- src/main/java/com/chocksaway/entity/Account.java
+++ src/main/java/com/chocksaway/entity/Account.java
@@ -18,18 +18,23 @@ public class Account {
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
